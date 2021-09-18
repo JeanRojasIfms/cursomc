e4c94065993a6ebe80e570373feb38232279d272
Jean Carlos Cabrera Rojas <jean.rojas@ifms.edu.br>
2021-09-17 21:11:43 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/br/edu/ifms/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ifms.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ifms.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
